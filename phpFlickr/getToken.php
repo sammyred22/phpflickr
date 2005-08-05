b664ed9741530787ab8c3cb6c95c896083c44e5b
dancoulter <dancoulter@33b6bdc0-2cd5-11df-bc59-2ffd2b2536e4>
2005-08-05 20:01:56 +0000

    Added version number

--- phpFlickr/getToken.php
+++ phpFlickr/getToken.php
@@ -1,5 +1,7 @@
 <?php
-    /* If you need your app to always login with the same user (to see your private
+    /* Released with phpFlickr 1.3.1 
+     *
+     * If you need your app to always login with the same user (to see your private
      * photos or photosets, for example), you can use this file to login and get a
      * token assigned so that you can hard code the token to be used.  To use this
      * use the phpFlickr::setToken() function whenever you create an instance of 
