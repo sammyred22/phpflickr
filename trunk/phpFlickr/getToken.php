1937ed8087492e1686d7a0dda451a17e9f9db879
dancoulter <dancoulter@33b6bdc0-2cd5-11df-bc59-2ffd2b2536e4>
2005-09-19 21:58:45 +0000

    Added a comment

--- trunk/phpFlickr/getToken.php
+++ trunk/phpFlickr/getToken.php
@@ -1,5 +1,5 @@
 <?php
-    /* Last updated with phpFlickr 1.3.1
+    /* Last updated with phpFlickr 1.4
      *
      * If you need your app to always login with the same user (to see your private
      * photos or photosets, for example), you can use this file to login and get a
@@ -11,6 +11,7 @@
     require_once("phpFlickr.php");
     $f = new phpFlickr("<api key>", "<secret>");
     
+    //change this to the permissions you will need
     $f->auth("read");
     
     echo "Copy this token into your code: " . $_SESSION['phpFlickr_auth_token'];
